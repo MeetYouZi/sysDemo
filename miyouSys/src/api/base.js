8d15ff1e15ef6cae4bd7c819f481209d8bd06f4f
meetyouzi <987176347@qq.com>
2019-04-09 14:09:40 +0800

    04091409

--- miyouSys/src/api/base.js
+++
@@ -1,9 +0,0 @@
-/**
- * 接口域名的管理
- */
-const base = {
-    url: 'http://119.23.229.131:8002/api/v1',
-    bd: 'http://xxxxx22222.com/api'
-}
-
-export default base;
--- miyouSys/src/store/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-  changeToken(state, token) {
-    state.token = token;
-    try {
-      localStorage.token = token;
-    } catch (e) {}
-  }
-};
